d0f53bdf502d7b546c3064099c33137e2d54ef4b
Raj Thakkar <38595922+CoffeeWithRJ@users.noreply.github.com>
2021-08-26 18:57:53 +0530

    REFS: #709 and Fixes: #709 CREATE queries
    OBJMaster and Object Tables Created

---
+++ SQL/Raj Thakkar/Group7/Create, Insert queries.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE BUS_RESERVATION
+
+USE BUS_RESERVATION
+
+--CREATE QUEARY
+
+CREATE TABLE OBJECT_MASTER
+(
+[Type_Id] INT PRIMARY KEY IDENTITY(1,1),
+[Type_Name] VARCHAR(20) NOT NULL
+)
+
+----------------------------------------------------------------------------------------------------------- 
+
+CREATE TABLE [OBJECT]
+(
+Obj_Id INT PRIMARY KEY IDENTITY(1,1),
+[Type_Id] INT NOT NULL,
+Obj_Name VARCHAR(30) NOT NULL,
+CONSTRAINT FK_Type_Id FOREIGN KEY([Type_Id]) REFERENCES OBJECT_MASTER([Type_Id])
+)
+
