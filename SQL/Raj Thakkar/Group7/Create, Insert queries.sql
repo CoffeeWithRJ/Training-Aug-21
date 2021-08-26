f1e5f00eea321fdca998c6f2a57e23b39e5c5a1f
DilipKumavat <73545464+DilipKumavat@users.noreply.github.com>
2021-08-26 19:04:27 +0530

    REFS : #710 and fixes #710 Create query
    Created USER_INFO Table and BUS Table

--- SQL/Raj Thakkar/Group7/Create, Insert queries.sql
+++ SQL/Raj Thakkar/Group7/Create, Insert queries.sql
@@ -29,3 +29,32 @@ CREATE TABLE SUB_LOCATION
     CONSTRAINT Chk_Loc_Obj_Id FOREIGN KEY (Loc_Obj_Id) REFERENCES OBJECT(Obj_Id)
 )
 
+------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE USER_INFO
+(
+[User_Id] INT PRIMARY KEY IDENTITY(1,1),
+First_Name VARCHAR(15) NOT NULL,
+Last_Name VARCHAR(15) NOT NULL,
+Contact_No VARCHAR(10) NOT NULL CONSTRAINT Chk_Contact CHECK(Contact_No like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') UNIQUE,
+Email_Address VARCHAR(30) NULL CONSTRAINT Chk_Email CHECK(Email_Address like '%__@_%.com') UNIQUE,
+User_Password VARCHAR(12) NOT NULL CONSTRAINT Chk_Password CHECK(LEN(User_Password)>5 AND LEN(User_Password)<=12),
+Dob DATE NOT NULL,
+Gender TINYINT NOT NULL,
+City VARCHAR(15) NOT NULL,
+CONSTRAINT Chk_Gender CHECK(Gender BETWEEN 1 AND 3)
+)
+
+---------------------------------------------------------------------------------------------------------------
+CREATE TABLE BUS
+(
+    Bus_Id INT PRIMARY KEY IDENTITY(1,1),
+    Bus_Plate_Number VARCHAR(15) CONSTRAINT chk_plateNumber_bus CHECK(Bus_Plate_Number LIKE 
+    '[A-Z][A-Z][0-9][0-9][A-Z][A-Z][0-9][0-9][0-9][0-9]') UNIQUE NOT NULL,
+    Bus_Type TINYINT NOT NULL,
+    AC_Type VARCHAR(10) DEFAULT 'Non-AC',
+    Total_Seat TINYINT NOT NULL,
+	CONSTRAINT Chk_Bus_Type CHECK(Bus_Type BETWEEN 8 AND 11)
+)
+
+------------------------------------------------------------------------------------------------------------------
