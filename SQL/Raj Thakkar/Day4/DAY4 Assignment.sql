52d1fe70b88d4e165fc158412af9082b5cdcb5af
Raj Thakkar <thakkarrj44@gmail.com>
2021-09-06 10:01:33 +0530

    REFS:371 and fixes:371 aggregate and rank functions with assignment

--- SQL/Raj Thakkar/Day4/DAY4 Assignment.sql
+++ SQL/Raj Thakkar/Day4/DAY4 Assignment.sql
@@ -1,22 +1,41 @@
---Assignment1
+--Write a query to rank employees based on their salary for a month
 SELECT RANK() OVER(ORDER BY Salary DESC) AS SRank, Salary FROM Employees
 
---Assignment 2
-SELECT * FROM (SELECT DENSE_RANK() OVER(ORDER BY Salary DESC) AS DRANK,* FROM Employees)T WHERE DRANK=4
+--Select 4th Highest salary from employee table using ranking function
+SELECT * FROM 
+(
+   SELECT DENSE_RANK() OVER(ORDER BY Salary DESC) AS DRANK,* 
+   FROM Employees
+)T
+WHERE DRANK=4
 
---Assignment 3
-SELECT DepartmentID, SUM(Salary) AS TotalSalary FROM Employees GROUP BY DepartmentID
+--Get department, total salary with respect to a department from employee table.
+SELECT DepartmentID
+,      SUM(Salary) AS TotalSalary 
+FROM Employees 
+GROUP BY DepartmentID
 
---Assignment4
+--Get department, total salary with respect to a department from employee table order by total salary descending
 SELECT DepartmentID, SUM(Salary) AS TotalSalary FROM Employees GROUP BY DepartmentID ORDER BY TotalSalary DESC
 
---Assignment5
-SELECT DepartmentID, MAX(Salary) AS MaxS FROM Employees GROUP BY DepartmentID ORDER BY MaxS ASC
+--Get department wise maximum salary from employee table order by salary ascending
+SELECT DepartmentID
+,      MAX(Salary) AS MaxS 
+FROM Employees 
+GROUP BY DepartmentID 
+ORDER BY MaxS ASC
 
---Assignment 6
-SELECT DepartmentID, MIN(Salary) AS MinS FROM Employees GROUP BY DepartmentID ORDER BY MinS ASC
+--Get department wise minimum salary from employee table order by salary ascending
+SELECT DepartmentID
+,      MIN(Salary) AS MinS 
+FROM Employees 
+GROUP BY DepartmentID 
+ORDER BY MinS ASC
 
---Assignment 7
-SELECT DepartmentID, SUM(Salary) AS TotSalary FROM Employees 
-GROUP BY DepartmentID HAVING SUM(Salary) > 50000
+--Select department, total salary with respect to a department from employee table where total salary greater than 50000 order by TotalSalary descending
+SELECT DepartmentID
+,      SUM(Salary) AS TotSalary 
+FROM Employees 
+GROUP BY DepartmentID 
+HAVING SUM(Salary) > 50000
 ORDER BY TotSalary DESC
